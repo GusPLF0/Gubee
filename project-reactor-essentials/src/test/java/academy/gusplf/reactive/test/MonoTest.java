b9e6b7c5dc51e70e06fb479822ddaa5cd90c7312
Gustavo P <gpedr56@gmail.com>
2023-05-31 15:26:21 -0300

    Começando com Mono

--- project-reactor-essentials/src/test/java/academy/gusplf/reactive/test/MonoTest.java
+++ project-reactor-essentials/src/test/java/academy/gusplf/reactive/test/MonoTest.java
@@ -2,12 +2,63 @@ package academy.gusplf.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
+/**
+ * Reactive Streams
+ * 1. Assincrona
+ * 2. Non-blocking
+ * 3. Backpressure
+ *
+ * Publisher = Cria os eventos
+ *     ^--(subscribe)--- Subscriber = Se inscreve para os eventos
+ *            ^-------- Subscription = Criada quando um subscriber se inscreve para um publisher
+ *
+ * De forma automatica, quando você se inscreve em um publisher é chamado um onSubscribe com um subscription
+ *
+ * O subscription é usado para gerencia backpressure (subscriber gerencia o relacionamento)
+ * **/
 public class MonoTest {
 
     @Test
-    public void test() {
-        log.info("Tudo funfando");
+    public void monoSubscriber() {
+        String name = "Gustavo";
+        Mono<String> mono = Mono.just(name)
+                .log();
+
+        mono.subscribe();
+        log.info("=====================================================");
+        StepVerifier.create(mono)
+                        .expectNext("Gustavo").verifyComplete();
+    }
+
+    @Test
+    public void monoSubscriberConsumer() {
+        String name = "Gustavo";
+        Mono<String> mono = Mono.just(name)
+                .log();
+
+        mono.subscribe(s -> log.info("Eae {}, como você está?", s));
+
+        log.info("=====================================================");
+
+        StepVerifier.create(mono)
+                        .expectNext("Gustavo").verifyComplete();
+    }
+    @Test
+    public void monoSubscriberConsumerError() {
+        String name = "Gustavo";
+        Mono<String> mono = Mono.just(name)
+                        .map(s -> {throw new RuntimeException("Testing with error");});
+
+        mono.subscribe(s -> log.info("Eae {}, como você está?", s), s -> log.error("Algo deu errado"));
+        mono.subscribe(s -> log.info("Eae {}, como você está?", s),Throwable::printStackTrace);
+
+        log.info("=====================================================");
+
+        StepVerifier.create(mono)
+                        .expectError(RuntimeException.class).verify();
     }
 }
